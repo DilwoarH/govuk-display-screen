c72b10abbea621b3d84afaef2e07848bf99a0e28
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2019-04-05 14:11:10 +0100

    Adds React App for EU exit screen

---
+++ eu-exit-screen/src/views/eu-exit-dashboard/EuExitScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './EuExitScreen.css';
+
+class EuExitScreen extends Component {
+  render() {
+    return (
+      <div className="EuExitScreen">
+        <h1>EuExitScreen</h1>
+      </div>
+    );
+  }
+}
+
+export default EuExitScreen;
