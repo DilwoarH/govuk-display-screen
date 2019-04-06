08380f2166d923dcf6ce5a0f51e74e243a4530d0
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2019-04-06 03:55:34 +0100

    adds route to package json

---
+++ eu-exit-screen/build/precache-manifest.7cf65fb4b07ebfe94b2587217b13de5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72c9820cadbd92bd5635",
+    "url": "/eu-exit/static/js/runtime~main.e4a4c4f5.js"
+  },
+  {
+    "revision": "93476c36f76fd5155a6f",
+    "url": "/eu-exit/static/js/main.ab088d68.chunk.js"
+  },
+  {
+    "revision": "989bc6dcef3bc42fc02d",
+    "url": "/eu-exit/static/js/2.ce5de2d1.chunk.js"
+  },
+  {
+    "revision": "93476c36f76fd5155a6f",
+    "url": "/eu-exit/static/css/main.a8b4215e.chunk.css"
+  },
+  {
+    "revision": "040fd80941bb9ac5dcf366599f81b592",
+    "url": "/eu-exit/index.html"
+  }
+];
